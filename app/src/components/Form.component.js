805a83b26be6a88eccea309e31c5993f8a132e0b
KoDim97 <kodim97@yandex.ru>
2020-03-28 18:22:22 +0300

    Fixed view form which was deleted #66

--- app/src/components/Form.component.js
+++ app/src/components/Form.component.js
@@ -1,16 +1,16 @@
 import {withFormik} from "formik";
 import React from "react";
 import PropTypes from 'prop-types';
-import {Form, Col, Row, FormGroup, Label, Input, FormText} from 'reactstrap';
+import {Form as FormStrap, Col, Row, FormGroup, Label, Input, FormText} from 'reactstrap';
 
 
 
-export function FormikForms(props) {
+export function Form(props) {
+    const { values, handleChange, onChange, touched, errors, handleSubmit, onClickDelete, numOfForms, keyValue} = props;
 
-    const { values, handleChange, onChange, touched, errors, handleSubmit, onClickDelete, numOfForms} = props;
     React.useEffect(() => {
         if (onChange) {
-            onChange(props.index, values);
+            onChange(keyValue, values);
         }
     }, [values]);
 
@@ -71,7 +71,7 @@ export function FormikForms(props) {
     if (numOfForms !== 1){
         deleteButtons = (
             <label className = "deleteButtonPlace"
-                   htmlFor = {"delete-button"+ (props.index + 1)}>
+                   htmlFor = {"delete-button"+ (keyValue + 1)}>
             </label>
         );
         statusDelete = {};
@@ -80,153 +80,158 @@ export function FormikForms(props) {
         statusDelete["disabled"] = "disabled";
     }
 
+
     return (
-        <Form id={"form"+(props.index + 1)}
-              className="forms"
-              onSubmit={handleSubmit}>
-            <Row>
-                <Col md={12}>
-                    <div className = "formHead">
-                        <button id = {"delete-button"+ (props.index + 1)}
-                                className="deleteButton"
-                                onClick={() => onClickDelete(props.index)}
-                                type="button"
-                                {...statusDelete}
-                        >
-                        </button>
-                        {deleteButtons}
-                    </div>
-                </Col>
-            </Row>
-            <Row form>
-                <Col md={12}>
-                    <FormGroup>
-                        <Label>Medication name </Label>
-                        <Input name="drugName"
-                               value={values.drugName}
-                               onChange={handleChange} />
-                        {touched.drugName && errors.drugName ?
-                            (
-                                <FormText color="red">
-                                    {errors.drugName}
-                                </FormText>
-                            ) : null}
-                    </FormGroup>
-                </Col>
-            </Row>
-            <Row form>
-                <Col md={4}>
-                    <FormGroup>
-                        <Label>Date from </Label>
-                        <Input name="dateFrom"
-                               type="date"
-                               onChange={handleChange}
-                               value={values.dateFrom}
-                        />
-                        {touched.dateFrom && errors.dateFrom ?
-                            (
-                                <FormText color="red">
-                                    {errors.dateFrom}
-                                </FormText>
-                            ) : null}
-                    </FormGroup>
-                </Col>
-                <Col md={4}>
-                     <FormGroup>
-                        <Label>Date to </Label>
-                        <Input name="dateTo"
-                               type="date"
-                               onChange={handleChange}
-                               value={values.dateTo}
-                        />
-                         {touched.dateTo && errors.dateTo ?
-                             (
-                                 <FormText color="red">
-                                     {errors.dateTo}
-                                 </FormText>
-                             ) : null}
-                    </FormGroup>
-                </Col>
-                <Col md={4}>
-                    <FormGroup>
-                        <Label>Frequency </Label>
-                        <Input type="select"
-                               name="dosage"
-                               onChange={handleChange}
-                               value={values.dosage}>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                            <option>6</option>
-                        </Input>
-                    </FormGroup>
-                </Col>
-            </Row>
-            <Row form>
-                <Col md={12}>
-                    {touched.dateTo && touched.dateFrom && !errors.dateFrom && !errors.dateTo && errors.endDate ?
-                        (
-                            <FormText id="longMessage" color="red">
-                                {errors.endDate}
-                            </FormText>
-                        ) : null}
-                </Col>
-            </Row>
-            <Row form>
-                <Col md={12}>
-                    <Label>Times taken</Label>
-                </Col>
-            </Row>
-            {layoutTimeListTag}
-            <Row form>
-                <Col md={4}>
-                    <FormGroup check>
-                        <Input name = "notifications"
-                               type="checkbox"
-                               id = {"notification-checkbox" + (props.index + 1)}
-                               onChange={handleChange}
-                               value={values.notification}
-                               />
-                        <label className = "notificationCheckbox"
-                               htmlFor = {"notification-checkbox" + (props.index + 1)}
-                        >
-                        </label>
-                        <Label >Remind me:</Label>
-                    </FormGroup>
-                </Col>
-                <Col md={4}>
-                    <Input type="select"
-                           name="remindTime"
-                           onChange={handleChange}
-                           value={values.remindTime}
-                           {...statusSelect}>
-                        <option value={0}>at the moment</option>
-                        <option value={5}>in 5 minutes</option>
-                        <option value={10}>in 10 minutes</option>
-                        <option value={15}>in 15 minutes</option>
-                        <option value={30}>in half-hour</option>
-                        <option value={60}>in 1 hour</option>
-                    </Input>
-                </Col>
-                <Col md={4}>
-                    <button className="formSubmit" type="submit"></button>
-                </Col>
-            </Row>
-            <Row form>
-                <Col md={12}>
-                    <FormGroup>
-                        <Label>Special instructions:</Label>
-                        <Input type="textarea"
-                               name="description"
-                               onChange={handleChange}
-                               rows={2} cols={48}>
-                        </Input>
-                    </FormGroup>
-                </Col>
-            </Row>
-        </Form>
+        <Row>
+            <Col>
+                <FormStrap id={"form"+(keyValue + 1)}
+                      className="forms"
+                      onSubmit={handleSubmit}>
+                    <Row>
+                        <Col md={12}>
+                            <div className = "formHead">
+                                <button id = {"delete-button"+ (keyValue + 1)}
+                                        className="deleteButton"
+                                        onClick={() => onClickDelete(keyValue)}
+                                        type="button"
+                                        {...statusDelete}
+                                >
+                                </button>
+                                {deleteButtons}
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row form>
+                        <Col md={12}>
+                            <FormGroup>
+                                <Label>Medication name </Label>
+                                <Input name="drugName"
+                                       value={values.drugName}
+                                       onChange={handleChange} />
+                                {touched.drugName && errors.drugName ?
+                                    (
+                                        <FormText color="red">
+                                            {errors.drugName}
+                                        </FormText>
+                                    ) : null}
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                    <Row form>
+                        <Col md={4}>
+                            <FormGroup>
+                                <Label>Date from </Label>
+                                <Input name="dateFrom"
+                                       type="date"
+                                       onChange={handleChange}
+                                       value={values.dateFrom}
+                                />
+                                {touched.dateFrom && errors.dateFrom ?
+                                    (
+                                        <FormText color="red">
+                                            {errors.dateFrom}
+                                        </FormText>
+                                    ) : null}
+                            </FormGroup>
+                        </Col>
+                        <Col md={4}>
+                            <FormGroup>
+                                <Label>Date to </Label>
+                                <Input name="dateTo"
+                                       type="date"
+                                       onChange={handleChange}
+                                       value={values.dateTo}
+                                />
+                                {touched.dateTo && errors.dateTo ?
+                                    (
+                                        <FormText color="red">
+                                            {errors.dateTo}
+                                        </FormText>
+                                    ) : null}
+                            </FormGroup>
+                        </Col>
+                        <Col md={4}>
+                            <FormGroup>
+                                <Label>Frequency </Label>
+                                <Input type="select"
+                                       name="dosage"
+                                       onChange={handleChange}
+                                       value={values.dosage}>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                    <option>6</option>
+                                </Input>
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                    <Row form>
+                        <Col md={12}>
+                            {touched.dateTo && touched.dateFrom && !errors.dateFrom && !errors.dateTo && errors.endDate ?
+                                (
+                                    <FormText id="longMessage" color="red">
+                                        {errors.endDate}
+                                    </FormText>
+                                ) : null}
+                        </Col>
+                    </Row>
+                    <Row form>
+                        <Col md={12}>
+                            <Label>Times taken</Label>
+                        </Col>
+                    </Row>
+                    {layoutTimeListTag}
+                    <Row form>
+                        <Col md={4}>
+                            <FormGroup check>
+                                <Input name = "notifications"
+                                       type="checkbox"
+                                       id = {"notification-checkbox" + (keyValue + 1)}
+                                       onChange={handleChange}
+                                       value={values.notification}
+                                />
+                                <label className = "notificationCheckbox"
+                                       htmlFor = {"notification-checkbox" + (keyValue + 1)}
+                                >
+                                </label>
+                                <Label >Remind me:</Label>
+                            </FormGroup>
+                        </Col>
+                        <Col md={4}>
+                            <Input type="select"
+                                   name="remindTime"
+                                   onChange={handleChange}
+                                   value={values.remindTime}
+                                   {...statusSelect}>
+                                <option value={0}>at the moment</option>
+                                <option value={5}>in 5 minutes</option>
+                                <option value={10}>in 10 minutes</option>
+                                <option value={15}>in 15 minutes</option>
+                                <option value={30}>in half-hour</option>
+                                <option value={60}>in 1 hour</option>
+                            </Input>
+                        </Col>
+                        <Col md={4}>
+                            <button className="formSubmit" type="submit"></button>
+                        </Col>
+                    </Row>
+                    <Row form>
+                        <Col md={12}>
+                            <FormGroup>
+                                <Label>Special instructions:</Label>
+                                <Input type="textarea"
+                                       name="description"
+                                       onChange={handleChange}
+                                       rows={2} cols={48}>
+                                </Input>
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                </FormStrap>
+            </Col>
+        </Row>
     );
 }
 
@@ -276,7 +281,7 @@ export default withFormik({
         return errors;
     },
     handleSubmit: () => {},
-})(FormikForms);
+})(Form);
 
 Form.propTypes = {
     children: PropTypes.node,
